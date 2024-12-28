cd5e43e7b8ad63d8d96ace23fc74084a9374c844
hellobyboy <helloworld010868@gmail.com>
2024-12-28 16:46:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+const express = require('express')
+
+const app = express()
+
+// const port = 4000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/login', (req, res) => {
+    res.send('hii kashaf you are log in ')
+})
+
+app.get('/logout', (req, res) => {
+    res.send('hii kashaf you are log out ')
+})
+
+app.get('/kashaf', (req, res) => {
+    res.send('hii kashaf ')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
+})
